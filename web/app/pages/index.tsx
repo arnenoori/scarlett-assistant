97c6098d7c2393d53ab13a9ee276e259d16393b7
Arne <arnenoori@gmail.com>
2024-04-25 15:17:39 -0700

    adding analytics

--- web/app/pages/index.tsx
+++ web/app/pages/index.tsx
@@ -1,14 +1,17 @@
 import type { NextPage } from 'next'
+import PlausibleProvider from 'next-plausible'
 import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>TOS Buddy</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    </div>
+    <PlausibleProvider domain="tosbuddy.com">
+      <div className="flex min-h-screen flex-col items-center justify-center py-2">
+        <Head>
+          <title>TOS Buddy</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+      </div>
+    </PlausibleProvider>
   )
 }
 
