4435dd52c2aa04707ae6605b917178ae53637a40
connorpobrien <54494032+connorpobrien@users.noreply.github.com>
2024-04-28 14:21:11 -0700

    chrome extension done

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,8 +1,11 @@
-document.getElementById('sendUrl').addEventListener('click', function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        var currentTab = tabs[0];
-        chrome.runtime.sendMessage({url: currentTab.url}, function(response) {
-            document.getElementById('apiResponse').textContent = response;
+document.addEventListener('DOMContentLoaded', function () {
+    var sendUrlButton = document.getElementById('sendUrl');
+    sendUrlButton.addEventListener('click', function() {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            var currentTab = tabs[0];
+            chrome.runtime.sendMessage({url: currentTab.url}, function(response) {
+                document.getElementById('apiResponse').textContent = response;
+            });
         });
     });
 });
