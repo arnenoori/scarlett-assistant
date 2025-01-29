582971e3ade26f2fe1738e78a27299e9fabc363c
Arne <arnenoori@gmail.com>
2025-01-29 14:17:45 -0800

    google analytics tag

--- web/app/pages/_app.tsx
+++ web/app/pages/_app.tsx
@@ -1,8 +1,28 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-G3ZTZL04TQ"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+      >
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-G3ZTZL04TQ');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
